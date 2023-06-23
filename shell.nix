0ede9932a448b0bc12e5045875b7f527e9aeaa1e
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2023-06-23 20:57:22 +0300

    1

---
+++ shell.nix
@@ -0,0 +1,21 @@
+with import <nixpkgs> {};
+let
+  my-python-packages = python-packages: [
+    python-packages.pip
+    python-packages.numpy
+    # python-packages.pytelegrambotapi
+  ];
+  my-python = python311.withPackages my-python-packages;
+in
+  pkgs.mkShell {
+    buildInputs = [
+      bashInteractive
+      my-python
+    ];
+    shellHook = ''
+      export PIP_PREFIX="$(pwd)/_build/pip_packages"
+      export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.11/site-packages:$PYTHONPATH" 
+      unset SOURCE_DATE_EPOCH
+    '';
+  }
+
